fc639be63353b67de390138fce5e5d0a7bd34520
sivan18-meet <sbmislovely@gmail.com>
2017-02-02 08:58:04 +0200

    pos

--- turtle_chat_view.py
+++ turtle_chat_view.py
@@ -22,14 +22,16 @@ from turtle_chat_widgets import TextInput
 class TextBox(TextInput):
 
     def draw_box(self):
+        self.pos= (-200,-200)
+        turtle.hideturtle()
         self.writer.hideturtle()
         self.writer=turtle.clone()
         self.writer.penup()
         self.writer.goto(self.pos)
         self.writer.pendown()
-        self.writer.goto(self.width,0)
+        self.writer.goto(self.width,-200)
         self.writer.goto(self.width,self.height)
-        self.writer.goto(0,self.height)
+        self.writer.goto(-200,self.height)
         self.writer.goto(self.pos)
         
             
